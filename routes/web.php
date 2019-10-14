b4e272293a1f28bb82de752ac1f8aef5a74adaa0
董俊 <418826102@qq.com>
2019-10-14 23:59:03 +0800

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StaticPagesController@index');
+
+Route::get('/help', 'StaticPagesController@help');
+
+Route::get('/about', 'StaticPagesController@about');
