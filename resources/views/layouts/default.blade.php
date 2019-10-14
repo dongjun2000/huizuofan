417146b056f1af2e618c451e80959999a8bafdb7
董俊 <418826102@qq.com>
2019-10-15 01:39:37 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', '会做饭') - 唯有美食与爱不可辜负！</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+        <ul class="navbar-nav justify-content-end">
+            <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container">
     @yield('content')
+</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+
+            <h1>Hello {{ config('app.name') }}</h1>
+            <p class="lead">
+                唯有美食与爱不可辜负！
+            </p>
+            <p>
+                一切，将从这里开始。
+            </p>
+            <p>
+                <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+            </p>
+
+    </div>
 @stop
