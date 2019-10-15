b2411097e2c44e2ddef660b55de2b3d24d6275b4
董俊 <418826102@qq.com>
2019-10-15 15:36:42 +0800

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,5 +3,36 @@
 @section('title', '注册')
 
 @section('content')
-<h1>注册</h1>
+    <div class="offset-md-2 col-md-8">
+        <div class="card">
+            <div class="card-header">
+                <h5 style="margin-bottom:0">注册</h5>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('users.store') }}" method="POST">
+                    <div class="form-group">
+                        <label for="name">名称：</label>
+                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">邮箱：</label>
+                        <input type="text" class="form-control" name="email" id="email" value="{{ old('email') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">密码：</label>
+                        <input type="password" class="form-control" name="password" id="password" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">确认密码：</label>
+                        <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @stop
