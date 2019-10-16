27aa234dd536182dcc38045fac6c2d657cc8081f
董俊 <418826102@qq.com>
2019-10-16 11:37:13 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            $this->username() => 'required|string',
+            'password'        => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // 该用户存在于数据库，且邮箱和密码相符合
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', Auth::user());
+        }
+        session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+        return redirect()->back()->withInput();
+    }
+
+    public function destory()
+    {
+
+    }
+
+    public function username()
+    {
+        return 'email';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,10 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destory')->name('logout');
+
 Route::resource('users', 'UsersController');
+
