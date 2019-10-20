c59ea0279dfe2030acf96d36f451a64cc509cbbc
董俊 <418826102@qq.com>
2019-10-20 17:51:50 +0800

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,6 +9,12 @@
                 @include('shared._user_info', ['user' => $user])
             </section>
 
+            <section class="stats mt-2">
+                @include('shared._stats', ['user' => $user])
+            </section>
+
+            <hr>
+
             <section class="status">
                 @if($statuses->count() > 0)
                     <ul class="list-unstyled">
